e86e654b56bd2c5a5a138883363e092921b86fcc
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-01-05 06:19:21 -0300

    HTML agregado

---
+++ figuras.js
@@ -0,0 +1,41 @@
+function perimetroCuadrado()
+{
+    let lados;
+    resultado = lados * 4;
+}
+function areaCuadradoRectangulo()
+{
+    let base;
+    let altura;
+    resultado = base * altura;
+}
+function perimetroRectangulo()
+{
+    let ladosLargos;
+    let ladosCortos;
+    resultado = ladosCortos + ladosLargos;
+}
+function perimetroTriangulo()
+{
+    let lado1;
+    let lado2;
+    let lado3;
+    resultado = lado1 + lado2 + lado3;
+}
+function areaTriangulo()
+{
+    let base;
+    let altura;
+    resultado = (base * altura) / 2;
+}
+function perimetroCirculo()
+{
+    let radio;
+    resultado = radio * 2;
+}
+function areaCirculo()
+{
+    let radio;
+    resultado = Math.PI * (radio**2);
+    parseFloat(resultado.toFixed(4))
+}
