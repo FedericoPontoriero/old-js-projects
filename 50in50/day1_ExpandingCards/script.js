33c9274ec41cd502f9dd6a03b512a83914d92e31
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-01-17 15:51:11 -0300

    Day 1 de 50in50

---
+++ 50in50/day1_ExpandingCards/script.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel'); // Captura todo lo que tiene la class panel en una Nodelist, parecida a un array
+
+panels.forEach(panel => 
+{ // Recorre la lista
+    panel.addEventListener('click', () => 
+    { // Escucha el click y corre una función
+        removeActiveClasses(); // Ejecuta la función
+        panel.classList.add('active'); // Agrega la clase active
+    })
+});
+
+function removeActiveClasses()
+{
+    panels.forEach(panel => 
+    { // Remueve el active
+        panel.classList.remove('active');
+    })
+};
