9051853d0dc9a43c103ad7abc1f0477e83e417c0
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-01-19 18:04:20 -0300

    Agregado comentarios para legibilidad

--- Curso práctico JS/promedioModaMediana/promedioPonderado.js
+++ Curso práctico JS/promedioModaMediana/promedioPonderado.js
@@ -1,3 +1,7 @@
+// Extrae el promedio ponderado de un conjunto de notas con sus respectivos créditos. Se obtiene multiplicando cada nota con su crédito correspondiente, se suma ese resultado y por último, se divide por la suma de todos los créditos.
+
+
+// Primero se establece el conjunto con el que se va a trabajar
 const notes =
 [
     {
@@ -17,25 +21,30 @@ const notes =
     },
 ];
 
+// Se recorre el array y se multiplica la nota obtenida con su correspondiente crédito
 const notesWithCredit = notes.map(function (noteObject)
 {
     return noteObject.note * noteObject.credit;
 });
 
+// Se suma el resultado anterior
 const sumOfNotesWithCredit = notesWithCredit.reduce(function (sum = 0, newVal)
 {
     return sum + newVal;
 });
 
+// Se recorre el array para obtener el valor de cada uno de los créditos
 const credits = notes.map (function (noteObject)
 {
     return noteObject.credit;
 });
 
+// Se suma el resultado obtenido
 const sumOfCredits = credits.reduce (function (sum = 0, newVal)
 {
     return sum + newVal;
 });
 
+// Se divide la suma de las notas con su crédito por la cantidad de créditos para obtener la moda ponderada del conjunto
 const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
 
