9e665813a8fdf0eb3fa8d7c41f35fb2616575203
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-02-04 11:35:18 -0300

    Reformateo

--- Curso práctico JS/análisisFinanciero/analisisFinanciero.js
+++ Curso práctico JS/análisisFinanciero/analisisFinanciero.js
@@ -1,28 +1,40 @@
 // Calcular monto y cantidad cuotas mensuales de una compra con tarjeta de credito,
 // a partir del total de la compra y la tasa de interes
 
-function calculateInteresRateInMonths(interesRate)
-{
-    return Math.pow(1 + (interesRate / 100), 1 / 12) - 1;
+function calculateInteresRateInMonths(interesRate) {
+  return Math.pow(1 + interesRate / 100, 1 / 12) - 1;
 }
 
-function calculateInstalmentsAmount(purchaseAmount, instalments, monthlyInteresRate) 
-{
-    const num = Math.pow(1 + monthlyInteresRate, instalments) * monthlyInteresRate * purchaseAmount;
-    const den = Math.pow(1 + monthlyInteresRate, instalments) - 1;
-    return num / den;
+function calculateInstalmentsAmount(
+  purchaseAmount,
+  instalments,
+  monthlyInteresRate
+) {
+  const num =
+    Math.pow(1 + monthlyInteresRate, instalments) *
+    monthlyInteresRate *
+    purchaseAmount;
+  const den = Math.pow(1 + monthlyInteresRate, instalments) - 1;
+  return num / den;
 }
 
-function calculate() 
-{
-    const purchaseAmount = Number(document.getElementById('purchaseAmount').value);
-    const interesRate = Number(document.getElementById('interesRate').value);
-    const instalments = Number(document.getElementById('instalments').value);
+function calculate() {
+  const purchaseAmount = Number(
+    document.getElementById("purchaseAmount").value
+  );
+  const interesRate = Number(document.getElementById("interesRate").value);
+  const instalments = Number(document.getElementById("instalments").value);
 
-    const monthlyInteresRate = calculateInteresRateInMonths(interesRate);
+  const monthlyInteresRate = calculateInteresRateInMonths(interesRate);
 
-    const instalmentsAmount = calculateInstalmentsAmount(purchaseAmount, instalments, monthlyInteresRate);
+  const instalmentsAmount = calculateInstalmentsAmount(
+    purchaseAmount,
+    instalments,
+    monthlyInteresRate
+  );
 
-    const result = document.getElementById('result');
-    result.innerHTML = `La cuota mensual a pagar es: ${(instalmentsAmount).toFixed(2)} por ${instalments} meses`;
+  const result = document.getElementById("result");
+  result.innerHTML = `La cuota mensual a pagar es: ${instalmentsAmount.toFixed(
+    2
+  )} por ${instalments} meses`;
 }
