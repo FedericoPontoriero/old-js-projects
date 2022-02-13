eb77548c5a868325c765c25191ee0b905705960e
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-02-13 01:47:10 -0300

    Agregado DOM manipulation

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -4,3 +4,23 @@ body.append("Hello World"); // To append an element, including strings
 
 document.createElement("div"); // To create an element
 body.append(div);
+div.textContent = "Hello World"; // Text including everything
+div.innerText = "Hello World"; // Only text
+
+div.innerHtml = "<strong>Hello World</strong>"; // To add HTML
+
+const strong = document.createElement("strong");
+strong.innerText = "Hello World";
+div.append(strong);
+body.append(div); // The same but more secure way of adding HTML
+
+const spanbye = document.querySelector("#bye");
+spanbye.remove(); // To remove
+
+spanbye.dataset.newName = "hi"; // To manipulate a data- of an element
+
+spanbye.classList.add("hi"); // For classes
+spanbye.classList.remove("hi");
+spanbye.classList.toggle("hi"); // It adds or removes depending if it exist or not
+
+spanbye.style.color = "red"; // To modify styles
