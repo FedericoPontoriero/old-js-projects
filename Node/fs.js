d84d64290bf89fa860496a66d0eef8fa4af01370
FedericoPontoriero <federico@DESKTOP-RKLC654.localdomain>
2022-02-18 09:42:35 -0300

    Agregado fs

--- Node/fs.js
+++ Node/fs.js
@@ -7,3 +7,28 @@ fs.mkdir(path.join(__dirname, "/test")),
 		if (err) throw err;
 		console.log("Folder created...");
 	};
+
+// Create and write to file
+fs.writeFile(
+	// It overwrites if the file already exists fs.appendFile to add
+	path.join(__dirname, "/test", "hello.txt"),
+	"Hello World!",
+	(err) => {
+		if (err) throw err;
+		console.log("File written to...");
+	}
+);
+// Read file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+	if (err) throw err;
+	console.log(data);
+});
+// Rename file from hello.txt to helloworld.txt
+fs.rename(
+	path.join(__dirname, "/test", "hello.txt"),
+	path.join(__dirname, "/test", "helloworld.txt"),
+	(err, data) => {
+		if (err) throw err;
+		console.log("File renamed...");
+	}
+);
