8aecbb86410ed6a4648d1e85e11700af9b86842d
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-02-20 20:01:30 -0300

    Movidos archivos

--- basic-react/src/TodoList.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
-import Todo from "./Todo";
-
-export default function TodoList({ todos, toggleTodo }) {
-	return todos.map((todo) => {
-		return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />;
-	});
-}
