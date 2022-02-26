7772d1a0df141715ef0e7167df9865630b1ef9b8
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-02-26 20:55:57 -0300

    Comentarios

--- Node/http.js
+++ Node/http.js
@@ -83,3 +83,4 @@ const server = http.createServer((req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+// Use express to streamline this whole process
