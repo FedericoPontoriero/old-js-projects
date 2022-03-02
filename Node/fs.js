e530ce7a7a0246522bed48708330600c86edfc55
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-03-02 02:26:38 -0300

    Refactorizado

--- Node/fs.js
+++ Node/fs.js
@@ -24,6 +24,7 @@ fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
 	console.log(data);
 });
 // Rename file from hello.txt to helloworld.txt
+
 fs.rename(
 	path.join(__dirname, "/test", "hello.txt"),
 	path.join(__dirname, "/test", "helloworld.txt"),
