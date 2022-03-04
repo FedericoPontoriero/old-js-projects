944b84c3e1b1b75de2107c02dbfa06609338e146
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-03-04 20:24:54 -0300

    Modificado servidor

--- Node/servidor.js
+++ Node/servidor.js
@@ -15,9 +15,9 @@
 // });
 
 // Con express
-const express = require('express');
-const colors = require('colors');
-const morgan = require('morgan');
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
 const server = express();
 
 // function logger(req, res, next) {
@@ -26,54 +26,57 @@ const server = express();
 // } Sin morgan
 
 // Settings
-server.set('appName', 'Express tutorial');
-server.set('port', 3000);
-server.set('view engine', 'ejs');
+server.set("appName", "Express tutorial");
+server.set("port", 3000);
+server.set("view engine", "ejs");
 
 // Middlewares
 server.use(express.json);
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 
 // Routes
-server.all('/user', (req, res, next) => {
-  res.send('finished');
-  next();
-})
-
-server.get('/', (req, res) => {
-  const data = [{name: 'Alberto'}, {name: 'Roberto'}, {name: 'Adalberto'}];
-  res.render('index.ejs', {personas: data});
-})
-
-server.get('/user', function (req, res) {
-  res.json({
-    name: 'Miguel',
-    lastName: 'Hernandez'
-  });
-  res.end;
-})
-
-server.post('/user/:id', function (req, res) {
-  console.log(req.body);
-  console.log(req.params);
-  res.send('recibido');
-  res.end;
-})
-
-server.put('/user/:id', function (req, res) {
-  console.log(req.body)
-  res.send(`User ${req.params.id} updated`);
-  res.end;
-})
-
-server.delete('/user/:id', function (req, res) {
-  res.send(`User ${req.params.id} deleted`);
-  res.end;
-})
-
-server.use(express.static('public'))
-server.listen(3000, () => {
-  console.log(app.get('appName'));
-  console.log(`Server on port ${server.get('port')}`.red);
+server.all("/user", (req, res, next) => {
+	res.send("finished");
+	next();
 });
 
+server.get("/", (req, res) => {
+	const data = [
+		{ name: "Alberto" },
+		{ name: "Roberto" },
+		{ name: "Adalberto" },
+	];
+	res.render("index.ejs", { personas: data });
+});
+
+server.get("/user", function (req, res) {
+	res.json({
+		name: "Miguel",
+		lastName: "Hernandez",
+	});
+	res.end;
+});
+
+server.post("/user/:id", function (req, res) {
+	console.log(req.body);
+	console.log(req.params);
+	res.send("recibido");
+	res.end;
+});
+
+server.put("/user/:id", function (req, res) {
+	console.log(req.body);
+	res.send(`User ${req.params.id} updated`);
+	res.end;
+});
+
+server.delete("/user/:id", function (req, res) {
+	res.send(`User ${req.params.id} deleted`);
+	res.end;
+});
+
+server.use(express.static("public"));
+server.listen(3000, () => {
+	console.log(app.get("appName"));
+	console.log(`Server on port ${server.get("port")}`.red);
+});
