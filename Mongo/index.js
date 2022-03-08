3929cfa13c7ec9f5949cf3a94d549076bab5be14
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-03-08 15:54:44 -0300

    Uri

--- Mongo/index.js
+++ Mongo/index.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-	"mongodb+srv://FedericoPontoriero:pontoriero95@cluster0.6xjug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+	"mongodb+srv://FedericoPontoriero:<password>@cluster0.6xjug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
