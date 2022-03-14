18ccec895108c4dc9cb268000bda0aa390cc0755
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-03-14 10:17:07 -0300

    Corregidos typos

--- Curso Typescript /script.ts
+++ Curso Typescript /script.ts
@@ -7,12 +7,12 @@ let myNumber: number = 22;
 
 let myBoolean: boolean = true;
 
-let myVar: any = 'cualquiera';
+let myVar: any = "any";
 myVar = 23;
 
 // Strings
 
-document.write(myNumber.toString())
+document.write(myNumber.toString());
 
 // Arrays
 
@@ -23,7 +23,7 @@ let anyArray: any[] = [];
 
 // Tuples
 
-let strNumTuple: [string, number] = ['Hola', 23];
+let strNumTuple: [string, number] = ["Hola", 23];
 
 // void, undefined, null
 
@@ -33,123 +33,101 @@ let myUndefined: undefined = undefined;
 
 // Functions
 
-function sum(num1: number, num2: number): number
-{
-    return num1 + num2;
+function sum(num1: number, num2: number): number {
+	return num1 + num2;
+}
+
+let mySum = function (num1: number | string, num2: number | string): number {
+	if (typeof num1 === "string") {
+		num1 = parseInt(num1);
+	}
+	if (typeof num2 === "string") {
+		num2 = parseInt(num2);
+	}
+	return num1 + num2;
 };
 
-let mySum = function(
-    num1: number | string, 
-    num2: number | string): number
-    {
-        if(typeof(num1) === 'string')
-        {
-            num1 = parseInt(num1);
-        }
-        if(typeof(num2) === 'string')
-        {
-            num2 = parseInt(num2);
-        }
-        return num1 + num2;
-    };
+function getName(firstName: string, lastName?: string): string {
+	// el ? indica que es opcional
+	if (lastName == undefined) {
+		return firstName;
+	}
+	return `${firstName} ${lastName}`;
+}
 
-function getName(firstName: string,
-     lastName?: string): string // el ? indica que es opcional
-    {
-        if(lastName == undefined)
-        {
-            return firstName;
-        }
-        return `${firstName} ${lastName}`;
-    };
-
-function myVoidFunction(): void
-{
-    return;
+function myVoidFunction(): void {
+	return;
 }
 
 // Interfaces
-interface IToDo
-{
-    title: string;
-    text: string;
+interface IToDo {
+	title: string;
+	text: string;
 }
 
-function showToDo(toDo: IToDo)
-{
-    console.log(`${toDo.title} - ${toDo.text}`);
+function showToDo(toDo: IToDo) {
+	console.log(`${toDo.title} - ${toDo.text}`);
 }
 
-let myToDo: IToDo = 
-{
-    title: 'Eat Dinner',
-    text: 'lorem'
+let myToDo: IToDo = {
+	title: "Eat Dinner",
+	text: "lorem",
 };
-
 
 showToDo(myToDo);
 
 // Clases
 
-class User
-{
-    name: string;
-    email: string;
-    age: number;
+class User {
+	name: string;
+	email: string;
+	age: number;
 
-    constructor(
-        name: string,
-        email: string, 
-        age: number)
-    {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
+	constructor(name: string, email: string, age: number) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
+	}
 
-    register()
-    {
-        console.log(`${this.name} is registered`);
-    }
-    showMeAge()
-    {
-        return this.age;
-    }
-    public ageInYears()
-    {
-        return this.age + ' years';
-    }
-    payInVoice()
-    {
-        console.log(`${this.name} paided invoice`);
-    }
-};
+	register() {
+		console.log(`${this.name} is registered`);
+	}
+	showMeAge() {
+		return this.age;
+	}
+	public ageInYears() {
+		return this.age + " years";
+	}
+	payInVoice() {
+		console.log(`${this.name} paided invoice`);
+	}
+}
 
-let juan = new User('juan', 'juan@mail.com', 28);
+let juan = new User("juan", "juan@mail.com", 28);
 
 console.log(juan.ageInYears());
 
-
-
-class Members extends User
-{
-    id!: number;
-    constructor(id: number, name: string, email: string, age: number)
-    {
-        super(name, email, age);
-    }
-    payInVoice(): void {
-        super.payInVoice();
-    }
+class Members extends User {
+	id!: number;
+	constructor(id: number, name: string, email: string, age: number) {
+		super(name, email, age);
+	}
+	payInVoice(): void {
+		super.payInVoice();
+	}
 }
 
-let miguel = new Members(1, 'miguel', 'miguel@mail.com', 24);
+let miguel = new Members(1, "miguel", "miguel@mail.com", 24);
 miguel.payInVoice;
 
 // Enums
 
-enum Role { ADMIN, READ_ONLY, AUTHOR };
+enum Role {
+	ADMIN,
+	READ_ONLY,
+	AUTHOR,
+}
 
 // Function types
 
-let functionType: (a:number, b:number) => number;
+let functionType: (a: number, b: number) => number;
